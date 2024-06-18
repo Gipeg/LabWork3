35829a483b6b195b2f4545764f721e00d77fbc5f
fisp <arenatkanovna@gmail.com>
2024-06-18 23:28:29 +0300

    Добавьте файлы проекта.

---
+++ LabWorkFunctions/Functions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Program;
+namespace LabWorkFunctions
+{
+    public static class Functions
+    {
+        public static double GetFactorial(double n)
+        {
+            if (n < 0)
+            {
+                return 0;
+            }
+            else 
+            {
+                return n * GetFactorial(n);                
+            }
+        }
+
+        public static double GetPower(double x, int n)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ LabWorkFunctions/Program.cs
@@ -0,0 +1,10 @@
+﻿using LabWorkFunctions;
+using System.ComponentModel.Design;
+using static LabWorkFunctions.Functions;
+namespace Function;
+
+static int Factorial(double n)
+{
+    Console.WriteLine("напишите число: ");
+    Console.WriteLine(GetFactorial(n));
+}
